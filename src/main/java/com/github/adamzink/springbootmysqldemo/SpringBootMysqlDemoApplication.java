29f259a1ace4de6acf8988f6d6400dc8586aa7ab
Adam Zink <adamzink55@gmail.com>
2018-04-22 14:34:34 -0400

    Initial commit from Spring Initializr

---
+++ src/main/java/com/github/adamzink/springbootmysqldemo/SpringBootMysqlDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.adamzink.springbootmysqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlDemoApplication.class, args);
+	}
+}
