51418647e4402568493be95fcbd2989e83370274
Adam Zink <adamzink55@gmail.com>
2018-07-03 18:42:12 -0400

    #6 - Add default method to convert Collection of models to Collection of responses

--- src/main/java/com/github/adamzink/springbootmysqldemo/converter/common/ModelConverter.java
+++ src/main/java/com/github/adamzink/springbootmysqldemo/converter/common/ModelConverter.java
@@ -1,9 +1,21 @@
 package com.github.adamzink.springbootmysqldemo.converter.common;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public interface ModelConverter<Q, M, S> {
 
     M requestToModel(Q request);
 
     S modelToResponse (M model);
+
+    default Collection<S> modelToResponse(final Iterable<M> models) {
+        assert models != null;
+
+        final Collection<S> responses = new ArrayList<S>();
+        models.forEach(model -> responses.add(modelToResponse(model)));
+
+        return responses;
+    }
 
 }
