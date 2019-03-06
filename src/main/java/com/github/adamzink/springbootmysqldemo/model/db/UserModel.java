f2198fa24142fa7e75962b078dd0d377a74ae514
adamzink <adamzink55@gmail.com>
2019-03-05 23:15:58 -0500

    Rename table to APP_USER to avoid conflicts with database reserved words

--- src/main/java/com/github/adamzink/springbootmysqldemo/model/db/UserModel.java
+++ src/main/java/com/github/adamzink/springbootmysqldemo/model/db/UserModel.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name="USER")
+@Table(name="APP_USER")
 public class UserModel {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
---
+++ src/main/resources/db/mysql/migration/V1_1__rename_to_APP_USER.sql
@@ -0,0 +1,1 @@
+rename table USER to APP_USER;
